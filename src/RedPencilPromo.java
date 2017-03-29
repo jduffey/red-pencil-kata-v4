8d6b4679fc328799be6565d6973f8a57e04e01d1
jduffey <jedduffey@gmail.com>
2017-03-29 10:34:24 -0400

    Create RedPencilPromo class with isPromoActive() method

---
+++ src/RedPencilPromo.java
@@ -0,0 +1,8 @@
+/**
+ * Created by jedduffey on 3/29/17.
+ */
+public class RedPencilPromo {
+    public boolean isPromoActive(Item testObject) {
+        return false;
+    }
+}
--- test/RedPencilPromoTest.java
+++ test/RedPencilPromoTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by jedduffey on 3/29/17.
  */
@@ -9,5 +11,12 @@ public class RedPencilPromoTest {
     @Test
     public void itemClassShouldExistAndTakeInputs(){
         Item testObject = new Item(100, 100, 60, 0, 100);
+    }
+
+    @Test
+    public void redPencilPromoClassShouldExistAndReturnFalseWhenConditionsForPromoAreNotMet(){
+        Item testObject = new Item(100, 100, 60, 0, 100);
+        RedPencilPromo testPromo = new RedPencilPromo();
+        assertEquals(false, testPromo.isPromoActive(testObject));
     }
 }
